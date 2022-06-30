d21be2e983cebbeb08b1868773edc9cf7f071dd6
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-06-30 18:05:55 +0000

    Added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from modules import* 
 import colorama 
 from colorama import *
 print(Fore.BLACK+Fore.GREEN,"\t \t RouterExpolit")
@@ -5,4 +6,6 @@ print("\n \t\t By RessurectedBird")
 print("\t 1.Arduino \t 2.Router")
 print("\t 3. Rasberrypi\t 4. ")
 option=int(input(">>>"*5))
+if option==1:
+    print("I'm in the If else")
 
