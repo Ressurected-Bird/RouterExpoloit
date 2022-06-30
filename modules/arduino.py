1cc40af77a026e2964a2e3700526263824e7a48b
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-06-30 12:02:36 +0000

    Deletd some files

--- modules/arduino.py
+++ modules/arduino.py
@@ -1,7 +1,7 @@
 try:
     import colorama as c
     from colorama import *
-    #import arduino-python3 as ar
+    import arduino-python3 as ar
     import time as t
 except Exception as e :
     print("Please install the Missing Library  ")
@@ -9,5 +9,3 @@ except Exception as e :
 def banner():
     print(Back.BLACK + Fore.GREEN, "Welcome to the Arduino Expoilt")
 
-banner()
-print(">"*20)
