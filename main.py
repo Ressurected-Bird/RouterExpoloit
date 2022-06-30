1f6c6e5c4dfdbd8bc8f6dd6e5bca322cb1ed18d5
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-06-30 11:57:52 +0000

    Added the  option

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import colorama 
 from colorama import *
 print(Fore.BLACK+Fore.GREEN,"\t \t RouterExpolit")
+print("\n \t\t By RessurectedBird")
+print("\t 1.Arduino \t 2.Router")
+print("\t 3. Rasberrypi\t 4. ")
 option=int(input(">>>"*5))
 
