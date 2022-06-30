efac18f650a03e95331e00e71a96950ef529da01
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-06-30 06:47:03 +0000

    Added the Menu Option

--- modules/arduino.py
+++ modules/arduino.py
@@ -1,7 +1,13 @@
 try:
     import colorama as c
-    import arduino-python3 as ar
+    from colorama import *
+    #import arduino-python3 as ar
     import time as t
 except Exception as e :
     print("Please install the Missing Library  ")
 
+def banner():
+    print(Back.BLACK + Fore.GREEN, "Welcome to the Arduino Expoilt")
+
+banner()
+print(">"*20)
