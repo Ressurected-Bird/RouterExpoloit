bdb17ca0bdb8894a9976b66f15b475e82365487f
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-07-01 13:40:16 +0000

    Added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from modules import* 
-import colorama 
-from colorama import *
+try:
+    from modules import *
+    from colorama import *
+    import colorama
+except Exception as e:
+    print(f'{e}')
 print(Fore.BLACK+Fore.GREEN,"\t \t RouterExpolit")
 print("\n \t\t By RessurectedBird")
 print("\t 1.Arduino \t 2.Router")
@@ -8,4 +11,7 @@ print("\t 3. Rasberrypi\t 4. ")
 option=int(input(">>>"*5))
 if option==1:
     print("I'm in the If else")
-
+if option==2:
+    print("I'm in the else condiont")
+if option==3:
+    print(" I'm in the third option ")
