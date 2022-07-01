cd154a551881f85ad63d4f0ebe8a38fde7a8e7bc
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-07-01 16:55:09 +0000

    Done some changes

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ print(Fore.BLACK+Fore.GREEN,"\t \t RouterExpolit")
 print("\n \t\t By RessurectedBird")
 print("\t 1.Arduino \t 2.Router")
 print("\t 3. Rasberrypi\t 4. ")
-option=int(input(">>>"*5))
+print("\n")
+option=int(input(">>>>>\t"))
 if option==1:
     print("I'm in the If else")
 if option==2:
