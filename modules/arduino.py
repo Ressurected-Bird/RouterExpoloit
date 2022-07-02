e74edc5541618a2d39c8c040027719fafa3146f5
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-07-02 12:01:39 +0000

    Arduino

--- modules/arduino.py
+++ modules/arduino.py
@@ -1,11 +1,14 @@
 try:
     import colorama as c
     from colorama import *
-    import arduino-python3 as ar
+    #import arduino-python3 as ar
     import time as t
+    import os as s
 except Exception as e :
     print("Please install the Missing Library  ")
 
 def banner():
     print(Back.BLACK + Fore.GREEN, "Welcome to the Arduino Expoilt")
 
+s.system("clear");
+banner();
