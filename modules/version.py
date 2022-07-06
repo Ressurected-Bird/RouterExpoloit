3d22c6fb4026a51d934dbde40dc7f4762563091b
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-07-06 03:25:54 +0000

    Added

---
+++ modules/version.py
@@ -0,0 +1,13 @@
+import sys
+def check():
+    version=sys.version
+    if version==sys.version:
+        print("Yo've installed the corrected version")
+        
+        
+    else:
+        os.system("sudo apt install python3")
+        os.system("sudo apt install pyton3-pip")
+        os.system("sudo apt install python-is-python3")
+
+
