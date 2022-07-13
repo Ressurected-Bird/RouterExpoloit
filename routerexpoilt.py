49cd456502be53478c957700d94cb69d8754a54d
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2022-07-13 08:53:25 +0000

    Added pyfiglet  module

--- routerexpoilt.py
+++ routerexpoilt.py
@@ -4,14 +4,17 @@ try:
     from colorama import *
     import colorama
     import time
+    import pyfiglet
 except Exception as e:
     print(f'{e}')
 while(1):
     try:
         check()
         print()
-        print(Fore.BLACK+Fore.GREEN,"\t \t RouterExpolit")
-        print("\n \t By RessurectedBird")
+        print(Fore.BLACK+Fore.GREEN)
+        log=pyfiglet=pyfiglet.figlet_format("RouterExpoilt", font ="digital")
+        print("\t",log)
+        print("\n By RessurectedBird")
         print("\n")
         print("\t1.Mimt \t2.Network Sniffer")
         print("\t3.Credentials Harvesting\t4.Exit ")
